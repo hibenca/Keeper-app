1b1dcea70f7dc8b1b9e6b513d213c3aaf38e2d3a
hibenca <hiben06@gmail.com>
2020-06-21 12:22:11 -0500

    Add delete function and button styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
-import { useState } from "react";
 
 function App() {
+  const [notes, setNotes] = useState([]);
 
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
 
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
 
-
-    const [newTask, setNewTask] = useState([]);
-
-    function addTask(note) {
-        setNewTask(newNote => {
-            return [...newNote, note]
-        })
-    }
-
-    return (
-        <div>
-            <Header />
-            <CreateArea
-                addTask={addTask}
-            />
-            {newTask.map((noteItem, index) => {
-                return <Note key={index} title={noteItem.title} content={noteItem.content} />
-            })
-            }
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
